601b68ceb2e3db0c05cb1bd5bbf328715a0ae5e4
zachary.zs <caoyuhezs@outlook.com>
2019-01-10 16:25:06 +0800

    Cloned the project from XDW, now added my file numvc-test.cpp

---
+++ numvc-test.cpp
@@ -0,0 +1,29 @@
+// My NuMVC test source file
+//    MuMVC, a heuristic algorithm to solve the Minimum Vertex Cover
+// Problem, raised by Cai Shaowei, 2013 in paper `NuMVC: An Efficient
+//  Local Search Algorithm for Minimum Vertex Cover`, url as :
+// https://arxiv.org/abs/1402.0584.
+// Re-implemented by Zachary-ZS.
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+/* parameters of the algorithm */
+#define MAXV 780
+#define MAXE 254000
+
+// To be set
+int cutoff_time;
+
+
+
+int main() {
+
+    // POJ
+
+
+
+    return 0;
+}

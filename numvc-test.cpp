c3d0cb25a5e6a0666a442a58c78daf80594744e0
zachary.zs <caoyuhezs@outlook.com>
2019-01-15 22:46:19 +0800

    Changed readme and updated partof codes.

--- numvc-test.cpp
+++ numvc-test.cpp
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <stack>
 #include <iostream>
+#include <set>
 #include <string.h>
 
 using namespace std;
@@ -15,6 +16,8 @@ using namespace std;
 /* parameters of the algorithm */
 #define MAXV 780
 #define MAXE 254000
+typedef set<int> Set;
+
 int V, E;           // |V| & |E| in this case
 
 // To be set
@@ -23,8 +26,9 @@ int threshold;      // Delicate w when \bar{w} reaches threshold
                     // NOTICE: Cai set it to 0.5*|V|
 int conf_change[MAXV];
 int dscore[MAXV];
-int G[MAXV][MAXV];  // Graph
-int edge_weight[MAXE];
+int G[MAXV][MAXV] = {0};  // Graph
+int edge_weight[MAXE] = {0};
+int timer_mod[MAXV]; // Modified time, for vertex choosing
 
 /* Data structures for the problem */
 struct Edge{
@@ -33,15 +37,35 @@ struct Edge{
     Edge(int aa, int bb) : a(aa), b(bb){}
 } edge[MAXE];
 
+Set C;
+Set bestC;
+Set uncovered;
+
+
 void init_(){
-    for (int i = 0; i < E; i++)
-        edge_weight[i] = 1;
 
-
-    for (int i = 0; i < V; i++) {
+    for (int i = 1; i <= V; i++) {
         conf_change[i] = 1;
+        dscore[i] = 0;
+        timer_mod[i] = 0;
+    }
+    for (int i = 1; i <= E; i++) {
+        edge_weight[i] = 1;
+        dscore[edge[i].a] += edge_weight[i];
+        dscore[edge[i].b] += edge_weight[i];
+        // At first, all the edges are uncovered
+        uncovered.insert(i);
     }
 
+
+
+}
+
+// Add vertex v into C and update other data accordingly.
+void add(int v){
+    C.insert(v);
+    dscore[v] = -dscore[v];
+    
 }
 
 
@@ -64,14 +88,14 @@ int main() {
             G[aa][bb] = 1;
             G[bb][aa] = 1;
         }
-        int numedge = 0;
+        int numedge = 1;
         for (int i = 1; i <= V; i++)
             for (int j = i + 1; j <= V; j++)
                 if(G[i][j]){
                     edge[numedge].a = i;
                     edge[numedge++].b = j;
                 }
-        E = numedge;
+        E = numedge - 1;
 
 
     }
